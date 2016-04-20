a5a45143947646661279e07de9d7da2e95364de3
Arkendorf <amonafk@yahoo.com>
2016-04-20 08:04:18 -0400

    ai

---
+++ enemy.lua
@@ -0,0 +1,80 @@
+playersSpotted = {{1, 1, 1, 1}}
+
+function newEnemy(x, y)
+  table.insert(enemies, {x, y, x, y, 10})
+  table.insert(enemyMove, {0, 0, 0})
+end
+
+function spotPlayers(i)
+  distanceToPlayers = {}
+  for char = 1, 4 do
+    distanceToPlayers[char] = {char, math.sqrt((chars[char][3] - enemies[i][1]) * (chars[char][3] - enemies[i][1]) + (chars[char][4] - enemies[i][2]) * (chars[char][4] - enemies[i][2]))}
+    if distanceToPlayers[char][2] < 512 then
+      playersSpotted[i][char] = 1
+    end
+  end
+end
+
+function getEnemyMoves(i)
+  validMoves = {}
+  if playersSpotted[i][1] == 0 and playersSpotted[i][2] == 0 and playersSpotted[i][3] == 0 and playersSpotted[i][4] == 0  then
+    return
+  end
+  distanceToPlayers = {}
+  for char = 1, 4 do
+    if playersSpotted[i][char] == 1 then
+      table.insert(distanceToPlayers, {char, math.sqrt((chars[char][3] - enemies[i][1]) * (chars[char][3] - enemies[i][1]) + (chars[char][4] - enemies[i][2]) * (chars[char][4] - enemies[i][2]))})
+    end
+  end
+  table.sort(distanceToPlayers, function(a, b) return a[2] < b[2] end)
+  for rowsDown = 0, #map - 1 do
+    for tilesAcross = 0, #map[1] - 1 do
+      moveDistance = math.sqrt((tilesAcross * 64 - enemies[i][1]) * (tilesAcross * 64 - enemies[i][1]) + (rowsDown * 64 - enemies[i][2]) * (rowsDown * 64 - enemies[i][2]))
+      if moveValid(enemies[i][1], enemies[i][2], tilesAcross * 64, rowsDown * 64) == true and noOverlap(enemies[i][1], enemies[i][2], tilesAcross * 64, rowsDown * 64) == true
+      and round(moveDistance / 64) <= 10 then
+        table.insert(validMoves, {tilesAcross * 64, rowsDown * 64, round(moveDistance / 64)})
+      end
+    end
+  end
+end
+
+function chooseEnemyMove(i)
+  enemies[i][5] = 10
+  if #validMoves < 1 then
+    return
+  end
+  if playersSpotted[i][1] == 0 and playersSpotted[i][2] == 0 and playersSpotted[i][3] == 0 and playersSpotted[i][4] == 0  then
+    return
+  end
+  tilesToMove = round((distanceToPlayers[1][2] - 256) / 64)
+    if tilesToMove > 0 then
+    table.sort(validMoves, function(a, b) return a[3] < b[3] end)
+    for range = tilesToMove, 10 do
+      for move = 1, #validMoves do
+        distanceToPlayer = math.sqrt((chars[distanceToPlayers[1][1]][3] - validMoves[move][1]) * (chars[distanceToPlayers[1][1]][3] - validMoves[move][1]) + (chars[distanceToPlayers[1][1]][4] - validMoves[move][2]) * (chars[distanceToPlayers[1][1]][4] - validMoves[move][2]))
+        if validMoves[move][3] == range and distanceToPlayer < distanceToPlayers[1][2] and moveValid(validMoves[move][1], validMoves[move][2], chars[distanceToPlayers[1][1]][3], chars[distanceToPlayers[1][1]][4]) then
+          enemies[i][5] = enemies[i][5] - validMoves[move][3]
+          enemies[i][3] = validMoves[move][1]
+          enemies[i][4] = validMoves[move][2]
+          enemyMove[i][3] = validMoves[move][3] * 16
+          enemyMove[i][1] = (validMoves[move][1] - enemies[i][1]) / enemyMove[i][3]
+          enemyMove[i][2] = (validMoves[move][2] - enemies[i][2]) / enemyMove[i][3]
+          return
+        end
+      end
+    end
+    for range = tilesToMove, 0, -1 do
+      for move = 1, #validMoves do
+        if validMoves[move][3] == range and distanceToPlayer < distanceToPlayers[1][2] and moveValid(validMoves[move][1], validMoves[move][2], chars[distanceToPlayers[1][1]][3], chars[distanceToPlayers[1][1]][4]) then
+          enemies[i][5] = enemies[i][5] - validMoves[move][3]
+          enemies[i][3] = validMoves[move][1]
+          enemies[i][4] = validMoves[move][2]
+          enemyMove[i][3] = validMoves[move][3] * 16
+          enemyMove[i][1] = (validMoves[move][1] - enemies[i][1]) / enemyMove[i][3]
+          enemyMove[i][2] = (validMoves[move][2] - enemies[i][2]) / enemyMove[i][3]
+          return
+        end
+      end
+    end
+  end
+end
--- main.lua
+++ main.lua
@@ -13,6 +13,8 @@ end
 
 function love.load()
 
+  require("enemy")
+
   love.window.setFullscreen(true)
 
   tiles = {}
@@ -45,11 +47,12 @@ function love.load()
   charMove = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
   enemies = {{256, 256, 256, 256, 10}}
   enemyMove = {{0, 0, 0}}
+  orderE = {}
 end
 
 function round(n, deci) deci = 10^(deci or 0) return math.floor(n*deci+.5)/deci end
 
-function moveValid(x1, y1, x2, y2)
+function noOverlap(x1, y1, x2, y2)
   for i = 1, 4 do
     if x2 == round(chars[i][1]) and y2 == round(chars[i][2]) then
       return false
@@ -61,9 +64,19 @@ function moveValid(x1, y1, x2, y2)
     end
   end
   distance = math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1))
+  return true
+end
+
+function enoughActions(x1, y1, x2, y2)
+  distance = math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1))
   if chars[selected][5] - round(distance / 64) < 0 then
     return false
   end
+  return true
+end
+
+function moveValid(x1, y1, x2, y2)
+  distance = math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1))
   for rowsDown = 0, #map - 1 do
     for tilesAcross = 0, #map[1] - 1 do
       if tileType[map[rowsDown + 1][tilesAcross + 1]] == 1 then
@@ -111,6 +124,17 @@ function movePlayer(i)
   end
 end
 
+function moveEnemy(i)
+  if enemyMove[i][3] > 0 then
+    enemies[i][1] = enemies[i][1] + enemyMove[i][1]
+    enemies[i][2] = enemies[i][2] + enemyMove[i][2]
+    enemyMove[i][3] = enemyMove[i][3] - 1
+  else
+    enemies[i][1] = enemies[i][3]
+    enemies[i][2] = enemies[i][4]
+  end
+end
+
 function love.update(dt)
   dtTotal = dtTotal + dt
   if love.keyboard.isDown("w") then
@@ -137,11 +161,20 @@ function love.update(dt)
     revealMap(i, 300)
   end
 
+  for i = 1, #enemies do
+    moveEnemy(i)
+  end
+
   if chars[1][5] == 0 and chars[2][5] == 0 and chars[3][5] == 0 and chars[4][5] == 0 then
     chars[1][5] = 10
     chars[2][5] = 10
     chars[3][5] = 10
     chars[4][5] = 10
+    for i = 1, #enemies do
+      spotPlayers(i)
+      getEnemyMoves(i)
+      chooseEnemyMove(i)
+    end
   end
 
   love.graphics.setLineWidth(math.sin(dtTotal) * 5)
@@ -154,7 +187,9 @@ end
 
 function love.mousepressed(mX, mY, button)
   if chars[selected][3] - chars[selected][1] < 1 and chars[selected][4] - chars[selected][2] < 0.5 then
-    if moveValid(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true then
+    if moveValid(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true
+    and noOverlap(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true
+    and enoughActions(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true then
       chars[selected][5] = chars[selected][5] - round(distance / 64)
       chars[selected][3] = round((mX - 32 + x) / 64) * 64
       chars[selected][4] = round((mY - 32 + y) / 64) * 64
@@ -177,20 +212,11 @@ function love.keypressed(key)
 end
 
 function love.draw()
-
-  orderE = {{scout, scout1, enemies[1][1] - x, enemies[1][2] - y - 32}}
-  table.sort(orderE, function(a, b) return a[4] < b[4] end)
-
   love.graphics.setColor(255, 255, 255)
   for rowsDown = 1, #map do
     for tilesAcross = 1, #map[1] do
       if mapRevealed[rowsDown][tilesAcross] == 1 then
         love.graphics.draw(tileset, tiles[map[rowsDown][tilesAcross]], (tilesAcross - 1) * 64 - x, (rowsDown - 1) * 64 - y)
-        for i = 1, #orderE do -- Enemies
-          if enemies[i][3] == tilesAcross * 64 - 64 and enemies[i][4] == rowsDown * 64 - 64 then
-            love.graphics.draw(orderE[i][1], orderE[i][2], orderE[i][3], orderE[i][4])
-          end
-        end
       end
     end
   end
@@ -198,11 +224,24 @@ function love.draw()
   orderC = {{scout, scout1, chars[1][1] - x, chars[1][2] - y - 32}, {scout, scout1, chars[2][1] - x, chars[2][2] - y - 32}, {scout, scout1, chars[3][1] - x, chars[3][2] - y - 32}, {scout, scout1, chars[4][1] - x, chars[4][2] - y - 32}}
   table.sort(orderC, function(a, b) return a[4] < b[4] end)
 
+  for i = 1, #enemies do
+    table.insert(orderE, {scout, scout1, enemies[i][1] - x, enemies[i][2] - y - 32})
+  end
+  table.sort(orderE, function(a, b) return a[4] < b[4] end)
+
   for i = 1, 4 do  -- Players
     love.graphics.draw(orderC[i][1], orderC[i][2], orderC[i][3], orderC[i][4])
   end
 
-  if moveValid(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true then
+  for i = 1, #orderE do -- Enemies
+    if mapRevealed[round((orderE[i][4] + y + 32) / 64)][round((orderE[i][3] + x) / 64)] == 1 then
+      love.graphics.draw(orderE[i][1], orderE[i][2], orderE[i][3], orderE[i][4])
+    end
+  end
+
+  if moveValid(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true
+  and noOverlap(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true
+  and enoughActions(round(chars[selected][1]), round(chars[selected][2]), round((mX - 32 + x) / 64) * 64, round((mY - 32 + y) / 64) * 64) == true then
     love.graphics.setColor(0, 255, 255, 125)
   else
     love.graphics.setColor(255, 0, 0, 125)
